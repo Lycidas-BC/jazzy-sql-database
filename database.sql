b39eec35d911224349f0606b37d7ffb1966d7a2f
Benji Copan <benji1220@gmail.com>
2021-06-29 23:32:02 -0500

    queries for making and populating tables

--- database.sql
+++ database.sql
@@ -3,3 +3,35 @@ CREATE TABLE "artists" (
     "artist_name" varchar(80) not null,
     "year_born" date
 );
+
+-- create artist table
+CREATE TABLE artist (
+    "id" serial PRIMARY KEY,
+    "name" varchar(80) NOT NULL,
+    "birthdate" date
+);
+
+-- create song table
+CREATE TABLE song (
+    "id" serial PRIMARY KEY,
+    "title" varchar(255) NOT NULL,
+    "length" varchar(10),
+    "released" date
+);
+
+-- for testing
+SELECT * FROM artist;
+SELECT * FROM song;
+
+-- populate artist table
+INSERT INTO artist ("name", "birthdate")
+	VALUES ('Ella Fitzgerald', '04-25-1917'),
+    ('Dave Brubeck', '12-06-1920'),
+    ('Miles Davis', '05-26-1926'),
+    ('Esperanza Spalding', '10-18-1984');
+
+-- populate song table
+INSERT INTO song ("title", "length", "released")
+	VALUES ('Take Five', '5:24', '1959-09-29'),
+	('So What', '9:22', '1959-08-17'),
+	('Black Gold', '5:17', '2012-02-01');
